695a73c08a4c5ffe17dd1443c111f032dd66b2ae
Coralee <coralee.long.partner@decathlon.com>
2023-05-05 19:32:29 +0200

    create basic layout structure

---
+++ src/components/Pages/About.svelte
@@ -0,0 +1,66 @@
+<h1>About</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo optio
+    corporis, illum fugiat nemo, amet, accusantium molestias eos sint a
+    praesentium maiores perferendis alias. Optio placeat expedita autem quia ut.
+    Atque ipsum ducimus dolor sunt est minima error sit eveniet, quia
+    repudiandae aperiam, velit possimus. Fugiat fugit quia eveniet, beatae odio
+    culpa aperiam consequatur dignissimos vel enim molestiae, obcaecati totam.
+    Optio, earum? Magnam numquam recusandae quam nihil quidem rerum repellat
+    nesciunt ipsum nam voluptate corporis excepturi provident, laboriosam odio
+    earum! Voluptas accusantium cumque iure? Vitae impedit saepe sed quisquam
+    dolor! Reprehenderit enim distinctio, itaque, porro in est, hic asperiores
+    laboriosam repellat ut autem commodi maxime molestiae iste esse et aperiam
+    iusto ex sunt laborum quae officiis a. Maxime, ea est. Animi accusamus esse
+    pariatur hic voluptate asperiores quisquam nihil ullam repellendus magnam
+    delectus optio aperiam expedita quo, debitis repudiandae nostrum deleniti.
+    Repellat, molestiae doloribus itaque aliquam illo neque commodi adipisci.
+    Ratione possimus dolorem sunt. Impedit animi accusantium itaque delectus
+    nemo temporibus magni unde, harum amet distinctio cumque explicabo tenetur
+    velit voluptates enim eum ab, placeat ad, nisi similique? Nobis, aliquam.
+    Voluptate, doloribus nemo quidem nobis unde nisi, natus recusandae cum
+    maiores, a suscipit atque necessitatibus. Aliquam in animi rem provident
+    enim architecto consequuntur, quia nihil culpa! Fuga inventore sint
+    mollitia! A fugit voluptatibus voluptas placeat at alias nulla ab. Sint,
+    esse praesentium voluptates sed eveniet nulla porro blanditiis nisi
+    aspernatur laboriosam officia, doloribus odio voluptatum, voluptate quisquam
+    aperiam pariatur facilis! Doloremque repellat error expedita nam placeat
+    nisi unde animi ipsam ipsum, quis earum exercitationem deleniti esse
+    perspiciatis accusamus, repellendus cupiditate quae commodi aliquid? Eos
+    earum aut iste fuga distinctio provident? Est, minus odit unde quia
+    perferendis porro nisi esse, eligendi corporis repellendus tempora
+    recusandae dolor dolorem corrupti laudantium quibusdam accusantium
+    laboriosam eos dolore beatae voluptatibus, dolores modi consequuntur.
+    Dolore, sunt. Nulla eos aperiam, voluptatum adipisci fuga atque veniam
+    dolores pariatur iusto distinctio provident voluptates ipsum aspernatur
+    vitae minima accusantium quos quasi fugiat unde dignissimos inventore magni
+    numquam doloribus officia. Explicabo. Asperiores illo deleniti totam
+    exercitationem praesentium sequi consequuntur at magnam repudiandae ipsa
+    vitae aliquam et, quae cupiditate iusto, accusamus, eaque doloremque sint.
+    Tempore veniam, architecto labore rem pariatur illo deleniti. Porro, quod
+    consectetur error corrupti facilis soluta hic. Hic odit ratione eveniet
+    soluta tempore tempora nostrum sapiente minima delectus asperiores obcaecati
+    accusamus cumque, error deleniti, repellat sequi tenetur maiores ea.
+    Deleniti debitis nam doloremque neque praesentium possimus, maiores quisquam
+    dolorum, doloribus quidem quia sit corrupti earum similique maxime, optio
+    suscipit eligendi accusantium nisi itaque amet. Laudantium minima quis
+    cumque. Maxime? Delectus molestiae, officia ipsam sint architecto
+    exercitationem nam asperiores obcaecati quis pariatur, quasi a enim hic
+    cupiditate assumenda amet? Sunt nam facere illum debitis assumenda veritatis
+    impedit dolor? Libero, velit. Accusamus perspiciatis dignissimos, voluptatem
+    iste similique fugiat quasi labore? Eius iste eveniet quam illo culpa sint
+    dolorum enim temporibus maxime totam, iure dolorem accusantium? Nam
+    molestias impedit earum maxime necessitatibus. Optio labore cum ex
+    laudantium qui deserunt dolores nesciunt pariatur quis ut. Tempore
+    explicabo, mollitia eaque quo eos, enim vero dolore dolorum voluptates velit
+    placeat quidem? Fugiat eaque fugit tenetur. Quia sapiente quidem commodi
+    dignissimos, iste quod corrupti quis blanditiis, veniam repudiandae optio ab
+    voluptates magni earum aspernatur accusamus. Natus aperiam, explicabo vitae
+    ratione nisi eos voluptatum sunt tenetur voluptates? Earum officia voluptas
+    mollitia asperiores ipsum? Dolor minus sit suscipit. Officia, debitis error
+    nostrum quos pariatur aperiam saepe incidunt. Illo, distinctio cum. Quidem
+    earum cumque beatae ratione cum pariatur velit. Assumenda optio
+    exercitationem eaque magnam, facere autem asperiores molestiae eum,
+    provident a nihil veniam corrupti maxime. Reprehenderit blanditiis debitis
+    asperiores ad aperiam deserunt in aliquam non, impedit, id, dicta a!
+</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import About from "/src/components/Pages/About.svelte"
+    import PageContainer from "/src/components/Layout/PageContainer.svelte"
+</script>
+
+<PageContainer><About /></PageContainer>
